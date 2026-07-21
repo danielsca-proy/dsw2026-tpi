0c8d0f307e42d2b8dcc6e89df6b8ccb8f2b0719b
Daniel <danielscalante4114@gmail.com>
2026-07-21 16:04:48 -0300

    Interfaz de especialidad añadido, sin validaciones

---
+++ Dsw2026Tpi.Application/Services/SpecialityService.cs
@@ -0,0 +1,59 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Application.Interfaces;
+using Dsw2026Tpi.CrossCutting.Exceptions;
+using Dsw2026Tpi.Domain.Entities;
+using Dsw2026Tpi.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Application.Services
+{
+    internal class SpecialityService : ISpecialityService
+    {
+        private readonly IPersistence _persistence;
+
+        public SpecialityService(IPersistence persistence)
+        {
+            _persistence = persistence;
+        }
+        public async Task<SpecialityModel.Response> Create(SpecialityModel.Request request)
+        {
+            var speciality = new Speciality(request.Name, request.Description);
+            await _persistence.Add<Speciality>(speciality);
+            return new SpecialityModel.Response(speciality.Id, speciality.Name, speciality.Description);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var speciality = await _persistence.GetById<Speciality>(id);
+
+            if (speciality is null)
+                throw new EntityNotFoundException("Speciality not found");
+
+            speciality.MarkAsDeleted();
+            await _persistence.Update<Speciality>(speciality);
+        }
+
+        public async Task<Pagination<SpecialityModel.Response>> GetAll(int pageSize, int pageIndex, string? name = null)
+        {
+            var specialities = await _persistence.Paginate<Speciality, string>(pageSize, pageIndex,
+                s => string.IsNullOrWhiteSpace(name) || s.Name.Contains(name), x => x.Name);
+
+            return specialities.Map(s => new SpecialityModel.Response(s.Id, s.Name, s.Description));
+        }
+
+        public async Task<SpecialityModel.Response> Update(Guid id, SpecialityModel.Request request)
+        {
+            var speciality = await _persistence.GetById<Speciality>(id);
+
+            if (speciality is null)
+                throw new EntityNotFoundException("Speciality not found");
+
+            speciality.UpdateDetails(request.Name, request.Description);
+            await _persistence.Update<Speciality>(speciality);
+
+            return new SpecialityModel.Response(speciality.Id, speciality.Name, speciality.Description);
+        }
+    }
+}
