b60f37d0c3f92bdc733b79039b4468618919285a
mendeznaza <nazarena.mendez@alu.frt.utn.edu.ar>
2026-07-25 15:39:06 -0300

    Update Dsw2026TpiDbContext.cs
    Elimine DB de doctors y specialiaty

--- Dsw2026Tpi.Data/Dsw2026TpiDbContext.cs
+++ Dsw2026Tpi.Data/Dsw2026TpiDbContext.cs
@@ -13,8 +13,7 @@ public class Dsw2026TpiDbContext: DbContext
     public DbSet<Dsw2026Tpi.Domain.Entities.AvailabilityRule> AvailabilityRules { get; set; }
     public DbSet<Dsw2026Tpi.Domain.Entities.AvailabilitySlot> AvailabilitySlots { get; set; }
     public DbSet<Dsw2026Tpi.Domain.Entities.Appointment> Appointments { get; set; }
-    public DbSet<Dsw2026Tpi.Domain.Entities.Doctor> Doctors { get; set; }
-    public DbSet<Dsw2026Tpi.Domain.Entities.Speciality> Specialities { get; set; }
+   
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
