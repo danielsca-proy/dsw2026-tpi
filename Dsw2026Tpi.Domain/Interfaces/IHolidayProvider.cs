ab272f072414b1250df44dd04dc0b6e2a1b8a25f
Mauro Benjamin Ibarra <MauroBenjaminIbarra@alu.frt.utn.edu.ar>
2026-08-03 19:57:40 -0300

    agregue el flujo alternativo de los dias feriados o no laborales

---
+++ Dsw2026Tpi.Domain/Interfaces/IHolidayProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Dsw2026Tpi.Domain.Interfaces;
+
+//Provee información sobre feriados y días no laborales en los que no deben generarse disponibilidades.
+
+public interface IHolidayProvider
+{
+    bool IsNonWorkingDay(DateOnly date);
+}
