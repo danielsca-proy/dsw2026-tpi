9edc789124c1523ec1d3119aa0eba7f89f8f4041
Daniel <danielscalante4114@gmail.com>
2026-08-03 16:16:50 -0300

    Se agrego rate limiting... me olvide de hacer otra rama para este feature...

--- Dsw2026Tpi.Api/Controllers/AppController.cs
+++ Dsw2026Tpi.Api/Controllers/AppController.cs
@@ -1,4 +1,6 @@
+using Dsw2026Tpi.Api.Configurations;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace Dsw2026Tpi.Api.Controllers;
 
@@ -6,6 +8,7 @@ namespace Dsw2026Tpi.Api.Controllers;
 /// Clase base para configuraciones generales de controladores
 /// </summary>
 [ApiController]
+[EnableRateLimiting(RateLimitPolicies.General)] //Para evitar tener que hacerlo en todos los controladores, lo hacemos aqui
 public abstract class AppController : ControllerBase
 {
 }
--- Dsw2026Tpi.Api/Program.cs
+++ Dsw2026Tpi.Api/Program.cs
@@ -24,6 +24,7 @@ public class Program
             builder.Services.AddSwaggerConfiguration();
             builder.Services.AddApplicationPersistence(builder.Configuration);
             builder.Services.AddAppCors(builder.Configuration);
+            builder.Services.AddAppRateLimiting(builder.Configuration);
             builder.Services.AddAppDependencies();
             builder.Services.AddControllers();
             builder.Services.AddHealthChecks();
@@ -49,10 +50,11 @@ public class Program
 
             app.UseCors();
             app.UseAuthentication();
+            app.UseRateLimiter();
             app.UseAuthorization();
 
             app.MapControllers();
-            app.MapHealthChecks("/health-check");
+            app.MapHealthChecks("/health-check").RequireRateLimiting(RateLimitPolicies.General);
 
             Log.Information("Aplicación iniciada correctamente");
 
