f28dbe46f822de9f24ebb0abc8a8b9ade9116acc
Daniel <danielscalante4114@gmail.com>
2026-08-03 15:06:47 -0300

    Problema de paginate (devolvia 0 tanto para 1 y 0)

--- Dsw2026Tpi.Data/PersistenceEf.cs
+++ Dsw2026Tpi.Data/PersistenceEf.cs
@@ -59,7 +59,7 @@ public class PersistenceEf: IPersistence
     public async Task<Pagination<T>> Paginate<T, TKey>(int pageSize, int pageIndex, Expression<Func<T, bool>> predicate, Expression<Func<T, TKey>> sortOrder, params string[] includes) where T : EntityBase
     {
         pageSize = Math.Abs(pageSize);
-        pageIndex = Math.Abs(pageIndex) == 0 ? 0 : Math.Abs(pageIndex) - 1;
+        pageIndex = Math.Max(0, pageIndex);
 
         var filtered = Include(_context.Set<T>(), includes)
                  .Where(predicate)
@@ -67,41 +67,12 @@ public class PersistenceEf: IPersistence
 
         var total = await filtered.CountAsync();
 
-        
-        async Task<Pagination<T>> GetPage(int skip, int take)
-        {
-            var data = await filtered.Skip(skip)
-                    .Take(take)
-                    .ToListAsync();
+        var data = await filtered
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
 
-            return new Pagination<T>(pageSize, pageIndex, total, data);
-        }
-        
-        //la pagina existe
-        if (total > pageSize * pageIndex)
-        {
-            return await GetPage(pageIndex * pageSize, pageSize);
-        }
-
-        //solo hay una pagina
-        if (total < pageSize)
-        {
-            return new Pagination<T>(pageSize, pageIndex, total, await filtered.ToListAsync());
-        }
-
-        var targetPageIndex = pageIndex - 1;
-
-        while (true)
-        {
-            if (total > targetPageIndex * pageSize)
-            {
-                return await GetPage(targetPageIndex * pageSize, pageSize);
-            }
-
-            targetPageIndex--;
-
-            if (targetPageIndex < 0) return new Pagination<T>(pageSize, 0, 0, []);
-        }
+        return new Pagination<T>(pageSize, pageIndex, total, data);
     }
 
     private static IQueryable<T> Include<T>(IQueryable<T> query, string[] includes) where T : EntityBase
