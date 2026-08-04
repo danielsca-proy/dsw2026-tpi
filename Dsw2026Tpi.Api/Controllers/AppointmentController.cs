4a2b7626c31fce91021b5c383c7224779e47b6e6
Daniel <danielscalante4114@gmail.com>
2026-08-04 13:09:11 -0300

    fix: arreglo de turnos que se podian agendar con doctores inactivos

--- Dsw2026Tpi.Api/Controllers/AppointmentController.cs
+++ Dsw2026Tpi.Api/Controllers/AppointmentController.cs
@@ -90,6 +90,7 @@ public class AppointmentController : AppController
     [ProducesResponseType(typeof(AppointmentModel.CreateResponse), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
 
     public async Task<IActionResult> Create([FromBody] AppointmentModel.CreateRequest request)
     {
